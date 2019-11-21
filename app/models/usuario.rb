d4f7d61246f1d8a2d9c31d05ce3318792b29a002
Danilo <didox_59@yahoo.com.br>
2019-11-21 10:38:56 -0300

    restringindo dados para perfil de usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,4 +1,6 @@
 class Usuario < ApplicationRecord
-	validates :email, presence: true, uniqueness: true
-	validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
+
+  belongs_to :perfil_usuario
 end
