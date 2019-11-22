0e6e4aef17ff7ef82962ede6f89a0e135c136ecf
Danilo <didox_59@yahoo.com.br>
2019-11-22 20:10:02 -0300

    novos

--- db/migrate/20191122224400_moeda_produtos.rb
+++ db/migrate/20191122224400_moeda_produtos.rb
@@ -1,4 +1,5 @@
 class MoedaProdutos < ActiveRecord::Migration[5.2]
   def change
+  	add_column :produtos, :moeda, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_181009) do
+ActiveRecord::Schema.define(version: 2019_11_22_224400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,6 +130,7 @@ ActiveRecord::Schema.define(version: 2019_11_22_181009) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "margem_site"
+    t.string "moeda"
     t.index ["partner_id"], name: "index_produtos_on_partner_id"
     t.index ["status_produto_id"], name: "index_produtos_on_status_produto_id"
   end
