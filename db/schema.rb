570008a5cc0e50c728138262f11d3f500d16d890
Rosi Volgarin <rosi.volgarin@gmail.com>
2019-11-30 11:35:30 +0100

    base de dados

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_174128) do
+ActiveRecord::Schema.define(version: 2019_11_30_091806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
