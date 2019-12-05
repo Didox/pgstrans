4207bce56195d5db319ef617603fb4c3c58b6ea1
Danilo <didox_59@yahoo.com.br>
2019-12-05 11:12:25 -0300

    send rest http

--- test.rb
+++ test.rb
@@ -8,7 +8,7 @@ data = RestClient.get 'https://parceiros.unitel.co.ao:8444/spgw/V2/getStatus', {
   sequenceId: 12346,
   saleSequenceId: 12343,
   terminalId: "00244TP00221",
-  token: "Y7fEbaU1os7yvwGPuIKTePxIzID0uS06vDd2UL+oGwPkqJT6Na23LXREW6U6VaK8L6Fl7GRWunMqkV2/SGxNAwF9AqecOUeNxp2f/AimDvCqmYV/axcEgKh2lrGlpnUBd1JHoT2tg7m31mqae/5MpphzQFUrJ21NXHGYGlNj0aw="
+  token: "C2RHhXiwWnHbheaTcd4Sfoemj5ToEPVJbIZOrDGPAhIeLxAJ/PMRQNtZhgfEo/t2v/aLm/RZK+qezh/YgrU3lMuoCUoUGh6/v6JJ9iFXI8e6Q+PxCmpmY2lJtRLgQFU2yqkzS7Ia0FgiE11NWo/bYLqVXTq4J1jq1s7p1ueItoQ="
 }
 
 debugger
