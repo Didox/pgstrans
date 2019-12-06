18b09ca903242424b2e52f04de1a3785a298d094
Rosi Volgarin <rosi.volgarin@gmail.com>
2019-12-06 09:51:28 +0100

    tudo certo

--- chaves/test.sh
+++ chaves/test.sh
@@ -13,7 +13,7 @@ sale_timestamp=`date +%s%3N`
 
 ## A chave privada
 private_key=/home/pgsadmin/PagasoAPP/pgstrans/chaves/rsapagasoprivkey.pem
-public_key=/home/pgsadmin/PagasoAPP/pgstrans/chaves/spgw_public.der
+public_key=/home/pgsadmin/PagasoAPP/pgstrans/chaves/rsapagasopubkey.der
 
 ## Criar a chave privada RSA de 2048 bits
 # -> openssl genpkey -algorithm RSA -outform DER -out test_priv_rsa_key.der  -pkeyopt rsa_keygen_bits:2048
