07d9cd822c8e310e90ae8188b04a1c8dbb7a8475
Danilo <didox_59@yahoo.com.br>
2019-12-07 06:42:13 -0300

    test envio chave

--- chaves/test.sh
+++ chaves/test.sh
@@ -90,12 +90,12 @@ echo "==========================[Enviar no request]=============================
 
 ##### Executar o pedido
 ##### echo -n "Parceiro [$agent_id], com vendedor [$seller_id] da loja [$store_id] e terminal [$terminal_id], está a tentar fazer um makeSale do produto [$product_id] de valor [$value] Akz para o msisdn [$client_msisdn]..."
-# tmp_make_sale_response=`curl -vs -X POST \
-# 	--max-time $max_time --connect-timeout $connect_timeout \
-#  	$make_sale_endpoint \
-#  	-H 'Cache-Control: no-cache' \
-#  	-d "$tmp_make_sale_request"`
+tmp_make_sale_response=`curl -vs -X POST \
+	--max-time $max_time --connect-timeout $connect_timeout \
+ 	$make_sale_endpoint \
+ 	-H 'Cache-Control: no-cache' \
+ 	-d "$tmp_make_sale_request"`
 
-# echo "==========================[Enviar no request]===================================="
-# echo $tmp_make_sale_response
-# echo "==========================[Enviar no request]===================================="
+echo "==========================[Enviar no request]===================================="
+echo $tmp_make_sale_response
+echo "==========================[Enviar no request]===================================="
