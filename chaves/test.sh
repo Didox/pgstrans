75be2fe40e84fac414b1317874ba0b94ef452f60
Danilo <didox_59@yahoo.com.br>
2019-12-07 06:40:45 -0300

    test envio chave

--- chaves/test.sh
+++ chaves/test.sh
@@ -88,14 +88,14 @@ echo "==========================[Enviar no request]=============================
 echo $tmp_make_sale_request
 echo "==========================[Enviar no request]===================================="
 
-# Executar o pedido
-# echo -n "Parceiro [$agent_id], com vendedor [$seller_id] da loja [$store_id] e terminal [$terminal_id], está a tentar fazer um makeSale do produto [$product_id] de valor [$value] Akz para o msisdn [$client_msisdn]..."
-tmp_make_sale_response=`curl -vs -X POST \
-	--max-time $max_time --connect-timeout $connect_timeout \
- 	$make_sale_endpoint \
- 	-H 'Cache-Control: no-cache' \
- 	-d "$tmp_make_sale_request"`
+##### Executar o pedido
+##### echo -n "Parceiro [$agent_id], com vendedor [$seller_id] da loja [$store_id] e terminal [$terminal_id], está a tentar fazer um makeSale do produto [$product_id] de valor [$value] Akz para o msisdn [$client_msisdn]..."
+# tmp_make_sale_response=`curl -vs -X POST \
+# 	--max-time $max_time --connect-timeout $connect_timeout \
+#  	$make_sale_endpoint \
+#  	-H 'Cache-Control: no-cache' \
+#  	-d "$tmp_make_sale_request"`
 
-echo "==========================[Enviar no request]===================================="
-echo $tmp_make_sale_response
-echo "==========================[Enviar no request]===================================="
+# echo "==========================[Enviar no request]===================================="
+# echo $tmp_make_sale_response
+# echo "==========================[Enviar no request]===================================="
