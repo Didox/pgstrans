b85570afd712ab5691964ca6283f7530180371e0
Danilo <didox_59@yahoo.com.br>
2019-12-10 07:01:19 -0300

    fix

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -3,6 +3,6 @@ class Produto < ApplicationRecord
   belongs_to :status_produto
   belongs_to :moeda
   validates :description, presence: true, uniqueness: true
-  validates :valor_compra_telemovel, :valor_compra_site, :valor_minimo_venda_telemovel, :valor_minimo_venda_site, :margem_telemovel, :margem_site, presence: true
+  validates :valor_compra_telemovel, :valor_minimo_venda_telemovel, :valor_minimo_venda_site, :margem_telemovel, :margem_site, presence: true
 end
 
