173d41e4c10bdafcfacd06e6aa9f5011560d0f18
Danilo <didox_59@yahoo.com.br>
2019-12-10 05:57:30 -0300

    configurando dados venda

--- db/migrate/20191210084847_sub_agente_usuario.rb
+++ db/migrate/20191210084847_sub_agente_usuario.rb
@@ -2,6 +2,5 @@ class SubAgenteUsuario < ActiveRecord::Migration[5.2]
   def change
   	add_reference :usuarios, :sub_agente, foreign_key: true
   	add_column :usuarios, :seller_id_parceiro, :bigint
-  	# remove_column :usuarios, :agent_id_parceiro
   end
 end
