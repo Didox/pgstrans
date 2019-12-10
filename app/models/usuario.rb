3c2c4a4086b2aa0057d97f23e1d0abe47f3958bf
Danilo <didox_59@yahoo.com.br>
2019-12-10 06:32:31 -0300

    novos

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,6 +1,7 @@
 class Usuario < ApplicationRecord
   has_many :matrix_users
   belongs_to :perfil_usuario
+  belongs_to :sub_agente
   
   validates :email, presence: true, uniqueness: true
   validates :nome, :email, :senha, presence: true
