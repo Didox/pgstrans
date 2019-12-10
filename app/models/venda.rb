bee873101b48f2719554fd2e78c1dbc8b9069163
Rosi Volgarin <rosi.volgarin@gmail.com>
2019-12-10 12:21:46 +0100

    valor to_i na venda

--- app/models/venda.rb
+++ app/models/venda.rb
@@ -17,7 +17,7 @@ class Venda < ApplicationRecord
     product_id = params[:unitel_produto_id].split("-").first
     telefone = params[:unitel_telefone]
 
-    venda = Venda.create(agent_id: AGENTE_ID, product_id: product_id, value: params[:valor], client_msisdn: telefone)
+    venda = Venda.create(agent_id: AGENTE_ID, product_id: product_id, value: params[:valor].to_i, client_msisdn: telefone)
 
     venda.store_id = usuario.sub_agente.store_id_parceiro
     venda.seller_id = usuario.sub_agente.seller_id_parceiro
