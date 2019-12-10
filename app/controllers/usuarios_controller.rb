6ee68fa8b46240d2ceb09feca5ed2fd01694e8cf
Danilo <didox_59@yahoo.com.br>
2019-12-10 06:35:02 -0300

    novos

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -69,6 +69,6 @@ class UsuariosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def usuario_params
-      params.require(:usuario).permit(:nome, :email, :senha, :perfil_usuario_id)
+      params.require(:usuario).permit(:nome, :email, :senha, :perfil_usuario_id, :sub_agente_id)
     end
 end
