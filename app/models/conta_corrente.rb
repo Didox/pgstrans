74ca633dc718f0582a1c8b5b4aaf16a86a2d5ebc
Danilo <didox_59@yahoo.com.br>
2019-12-12 06:42:08 -0300

    saldo

--- app/models/conta_corrente.rb
+++ app/models/conta_corrente.rb
@@ -3,9 +3,15 @@ class ContaCorrente < ApplicationRecord
   belongs_to :lancamento
   belongs_to :banco
 
+  before_validation :preenche_padrao
+
   after_save :atualiza_saldo
 
   private
+
+  def preenche_padrao
+  	self.data_alegacao_pagamento = Time.zone.now
+  end
 
   def atualiza_saldo
   	ContaCorrente.where(id: self.id).update_all(data_ultima_atualizacao_saldo: Time.zone.now, saldo_atual: ContaCorrente.where(usuario_id: usuario).sum(:valor))
