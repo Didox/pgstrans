463d7259ea5c012c3ca7705ae9aaa450f604fe0f
Danilo <didox_59@yahoo.com.br>
2019-12-13 06:26:05 -0300

    correcao

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,4 +1,5 @@
 class Partner < ApplicationRecord
-	has_many :return_code_api
-	validates :name, presence: true, uniqueness: true
+  has_many :return_code_api
+  validates :name, presence: true, uniqueness: true
+  belongs_to :status_parceiro
 end
