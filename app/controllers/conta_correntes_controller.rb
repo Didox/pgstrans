74362c327a75cb9fa1a08c447e6609c2fe5b177e
Rosi Volgarin <rosi.volgarin@gmail.com>
2019-12-23 01:29:22 +0100

    busca em conta corrente

--- app/controllers/conta_correntes_controller.rb
+++ app/controllers/conta_correntes_controller.rb
@@ -6,6 +6,14 @@ class ContaCorrentesController < ApplicationController
   def index
     if usuario_logado.admin?
       @conta_correntes = ContaCorrente.all
+      @conta_correntes = @conta_correntes.joins(:usuario)
+      @conta_correntes = ContaCorrente.all.reorder("updated_at desc")
+      @conta_correntes = @conta_correntes.where("conta_correntes.data_alegacao_pagamento >= ?", params[:data_alegacao_pagamento].to_datetime.beginning_of_day) if params[:data_alegacao_pagamento].present?
+      @conta_correntes = @conta_correntes.where("conta_correntes.data_ultima_atualizacao_saldo <= ?", params[:data_ultima_atualizacao_saldo].to_date.end_of_day) if params[:data_ultima_atualizacao_saldo].present?
+      @conta_correntes = @conta_correntes.where("usuarios.nome ilike '%#{params[:nome]}%'") if params[:nome].present?
+
+      options = {page: params[:page] || 1, per_page: 10}
+      @conta_correntes = @conta_correntes.paginate(options)
     else
       @conta_correntes = ContaCorrente.where(usuario: usuario_logado)
     end
