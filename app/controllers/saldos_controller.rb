81c87e21249b3f6cbaee59ef00a2afeef8a33f16
Danilo <didox_59@yahoo.com.br>
2019-12-30 08:12:23 -0300

    novs

--- app/controllers/saldos_controller.rb
+++ app/controllers/saldos_controller.rb
@@ -1,15 +1,10 @@
 class SaldosController < ApplicationController
   def index
-  	if usuario_logado.admin?
-      @conta_correntes = ContaCorrente.all
-      @conta_correntes = @conta_correntes.joins("inner join usuarios on usuarios.id = conta_correntes.usuario_id")
-      @conta_correntes = @conta_correntes.reorder("updated_at desc")
-      @conta_correntes = @conta_correntes.where("conta_correntes.data_alegacao_pagamento >= ?", params[:data_alegacao_pagamento].to_datetime.beginning_of_day) if params[:data_alegacao_pagamento].present?
-      @conta_correntes = @conta_correntes.where("conta_correntes.data_ultima_atualizacao_saldo <= ?", params[:data_ultima_atualizacao_saldo].to_date.end_of_day) if params[:data_ultima_atualizacao_saldo].present?
-      @conta_correntes = @conta_correntes.where("usuarios.nome ilike '%#{params[:nome]}%'") if params[:nome].present?
-    else
-      @conta_correntes = ContaCorrente.where(usuario: usuario_logado)
-    end
+    @conta_correntes = ContaCorrente.where(usuario: usuario_logado)
+    @conta_correntes = @conta_correntes.joins("inner join usuarios on usuarios.id = conta_correntes.usuario_id")
+    @conta_correntes = @conta_correntes.reorder("updated_at desc")
+    @conta_correntes = @conta_correntes.where("conta_correntes.data_ultima_atualizacao_saldo >= ?", params[:data_alegacao_pagamento_inicio].to_datetime.beginning_of_day) if params[:data_alegacao_pagamento_inicio].present?
+    @conta_correntes = @conta_correntes.where("conta_correntes.data_ultima_atualizacao_saldo <= ?", params[:data_alegacao_pagamento_fim].to_date.end_of_day) if params[:data_alegacao_pagamento_fim].present?
 
     options = {page: params[:page] || 1, per_page: 10}
     @conta_correntes = @conta_correntes.paginate(options)
