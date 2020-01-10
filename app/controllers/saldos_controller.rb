78889ed4b2c40677d9934fff4dd17d22d1c065af
Danilo <didox_59@yahoo.com.br>
2020-01-10 07:02:57 -0300

    fix

--- app/controllers/saldos_controller.rb
+++ app/controllers/saldos_controller.rb
@@ -3,8 +3,9 @@ class SaldosController < ApplicationController
     @conta_correntes = ContaCorrente.where(usuario: usuario_logado)
     @conta_correntes = @conta_correntes.joins("inner join usuarios on usuarios.id = conta_correntes.usuario_id")
     @conta_correntes = @conta_correntes.reorder("updated_at desc")
-    @conta_correntes = @conta_correntes.where("conta_correntes.data_ultima_atualizacao_saldo >= ?", params[:data_alegacao_pagamento_inicio].to_datetime.beginning_of_day) if params[:data_alegacao_pagamento_inicio].present?
-    @conta_correntes = @conta_correntes.where("conta_correntes.data_ultima_atualizacao_saldo <= ?", params[:data_alegacao_pagamento_fim].to_date.end_of_day) if params[:data_alegacao_pagamento_fim].present?
+    
+    @conta_correntes = @conta_correntes.where("conta_correntes.data_alegacao_pagamento >= ?", params[:data_alegacao_pagamento_inicio].to_datetime.beginning_of_day) if params[:data_alegacao_pagamento_inicio].present?
+    @conta_correntes = @conta_correntes.where("conta_correntes.data_alegacao_pagamento <= ?", params[:data_alegacao_pagamento_fim].to_date.end_of_day) if params[:data_alegacao_pagamento_fim].present?
 
     options = {page: params[:page] || 1, per_page: 10}
     @conta_correntes = @conta_correntes.paginate(options)
