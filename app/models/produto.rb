2bc54f31bd85c9124489f6e0cdf5c203470a579c
Danilo <didox_59@yahoo.com.br>
2020-01-24 07:15:12 -0300

    nova venda zaptv

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -4,5 +4,10 @@ class Produto < ApplicationRecord
   belongs_to :moeda
   validates :description, presence: true, uniqueness: { scope: [:description, :partner_id] }
   validates :valor_compra_telemovel, presence: true
+
+  def valor_minimo_venda_site
+  	return super if super.present?
+  	return self.valor_compra_telemovel
+  end
 end
 
