872cfc42266e8fbd59a7b58e5b1e61e9aa64ee06
Danilo <didox_59@yahoo.com.br>
2020-02-18 08:53:26 -0300

    fix

--- app/models/relatorio_conciliacao_zaptv.rb
+++ app/models/relatorio_conciliacao_zaptv.rb
@@ -1,3 +1,15 @@
 class RelatorioConciliacaoZaptv < ApplicationRecord
   belongs_to :partner
+
+  def self.to_csv
+    attributes = %w{operation_code source_reference product_code quantity date_time type_data total_price status unit_price}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |user|
+        csv << attributes.map{ |attr| user.send(attr) }
+      end
+    end
+  end
 end
