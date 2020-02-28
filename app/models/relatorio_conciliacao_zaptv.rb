0a3a9bbe399f45099db3b9bf495ed0e4079bef1f
Danilo <didox_59@yahoo.com.br>
2020-02-28 06:10:20 -0300

    novos

--- app/models/relatorio_conciliacao_zaptv.rb
+++ app/models/relatorio_conciliacao_zaptv.rb
@@ -2,7 +2,7 @@ class RelatorioConciliacaoZaptv < ApplicationRecord
   belongs_to :partner
 
   def self.to_csv
-    attributes = %w{operation_code source_reference product_code quantity date_time type_data total_price status unit_price zappis}
+    attributes = %w{operation_code source_reference product_code product_name quantity date_time type_data total_price status unit_price numero_cartao}
 
     CSV.generate(headers: true) do |csv|
       csv << attributes
@@ -13,8 +13,12 @@ class RelatorioConciliacaoZaptv < ApplicationRecord
     end
   end
 
-  def zappis
-    self.vendas.map { |venda| JSON.parse(venda.request_send)["zappi"] }.join(",")
+  def product_name
+    Produto.where(produto_id_parceiro: self.product_code, partner_id: self.partner_id).map{|p| p.nome}.join(" ")
+  end
+
+  def numero_cartao
+    self.vendas.map { |venda| JSON.parse(venda.request_send)["zappi"] }.join(" ")
   rescue 
     ""
   end
