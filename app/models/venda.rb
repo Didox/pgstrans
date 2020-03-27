2d19828a74ec32f216ad19925bb8120aa035c551
Danilo <didox_59@yahoo.com.br>
2020-03-27 07:21:49 -0300

    novo

--- app/models/venda.rb
+++ app/models/venda.rb
@@ -36,76 +36,6 @@ class Venda < ApplicationRecord
     self.response_get.scan(/RequestId.*?RequestId>/).first.scan(/>.*?</).first.scan(/\d/).join("") rescue ""
   end
 
-  def status_movicel
-    return if self.partner.name.downcase != "movicel"
-
-    require 'openssl'
-
-    #producao
-    agent_key = "3958CFE4F3A8DB09B76B9C0D26A19F6D69E1F6DC3BC5779EB84CB866DD939DF4"
-    url_service = "https://ws.movicel.co.ao:10073"
-
-    #homologacao
-    # agent_key = "5CF0AC45050B030B9E3A5FC14A5D7C8B609B2BDD40119B5C32539E1F3B6CEF7F"
-    # url_service = "http://wsqa.movicel.co.ao:10071"
-
-    user_id = "TivTechno"
-    request_id = self.request_id
-
-    pass = `AGENTKEY='#{agent_key}' USERID='#{user_id}' REQUESTID='#{request_id}' ./chaves/movicell/ubuntu/encripto`
-    # pass = `AGENTKEY='#{agent_key}' USERID='#{user_id}' REQUESTID='#{request_id}' ./chaves/movicell/mac/encripto`
-    pass = pass.strip
-
-    body = "
-      <soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:int=\"http://ws.movicel.co.ao/middleware/adapter/DirectTopup/interface\" xmlns:mid=\"http://schemas.datacontract.org/2004/07/Middleware.Common.Common\" xmlns:mid1=\"http://schemas.datacontract.org/2004/07/Middleware.Adapter.DirectTopup.Resources.Messages.DirectTopupAdapter\">
-         <soapenv:Header>
-            <int:QueryTransactionReqHeader>
-               <mid:RequestId>#{request_id}</mid:RequestId>
-               <mid:Timestamp>#{Time.zone.now.strftime("%Y-%m-%d")}</mid:Timestamp>
-               <!--Optional:-->
-               <mid:SourceSystem>#{user_id}</mid:SourceSystem>
-               <mid:Credentials>
-                  <mid:User>#{user_id}</mid:User>
-                  <mid:Password>#{pass}</mid:Password>
-               </mid:Credentials>
-               <!--Optional:-->
-               <mid:Attributes>
-                  <!--Zero or more repetitions:-->
-                  <mid:Attribute>
-                     <mid:Name>?</mid:Name>
-                     <mid:Value>?</mid:Value>
-                  </mid:Attribute>
-               </mid:Attributes>
-            </int:QueryTransactionReqHeader>
-         </soapenv:Header>
-         <soapenv:Body>
-            <int:QueryTransactionReq>
-               <!--Optional:-->
-               <int:QueryTransactionReqBody>
-                  <mid1:TransactionNumber>?</mid1:TransactionNumber>
-               </int:QueryTransactionReqBody>
-            </int:QueryTransactionReq>
-         </soapenv:Body>
-      </soapenv:Envelope>
-    "
-
-    url = "#{url_service}/DirectTopupService/Topup/"
-    uri = URI.parse(URI.escape(url))
-    request = HTTParty.post(uri, 
-      :headers => {
-        'Content-Type' => 'text/xml;charset=UTF-8',
-        'SOAPAction' => 'http://ws.movicel.co.ao/middleware/adapter/DirectTopup/interface/DirectTopupService_Outbound/QueryTransaction',
-      },
-      :body => body
-    )
-
-    if (200...300).include?(request.code.to_i)
-      # return request.body
-      return Nokogiri::XML(request.body).children.children.children.children.children.children.text rescue nil
-      # return "#{request_id} - #{Nokogiri::XML(request.body).children.children.children.children.children.text}" rescue nil
-    end
-  end
-
   def carregamento_venda_zaptv
     host = "http://10.151.59.196"
     url = "#{host}/ao/echarge/pagaso/dev/carregamento/#{self.request_id}"
@@ -246,6 +176,80 @@ class Venda < ApplicationRecord
       end
 
       return venda
+    end
+  end
+
+   def status_movicel
+    return if self.partner.name.downcase != "movicel"
+
+    require 'openssl'
+
+    parceiro = Partner.where("lower(slug) = 'movicel'").first
+    parametro = Parametro.where(partner_id: parceiro.id).first
+
+    if Rails.env == "development"
+      url_service = parametro.url_integracao_desenvolvimento
+      agent_key = parametro.agent_key_movicel_desenvolvimento
+      user_id = parametro.user_id_movicel_desenvolvimento
+    else
+      url_service = parametro.url_integracao_producao
+      agent_key = parametro.agent_key_movicel_producao
+      user_id = parametro.user_id_movicel_producao
+    end
+    
+    request_id = self.request_id
+
+    pass = `AGENTKEY='#{agent_key}' USERID='#{user_id}' REQUESTID='#{request_id}' ./chaves/movicell/ubuntu/encripto`
+    # pass = `AGENTKEY='#{agent_key}' USERID='#{user_id}' REQUESTID='#{request_id}' ./chaves/movicell/mac/encripto`
+    pass = pass.strip
+
+    body = "
+      <soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:int=\"http://ws.movicel.co.ao/middleware/adapter/DirectTopup/interface\" xmlns:mid=\"http://schemas.datacontract.org/2004/07/Middleware.Common.Common\" xmlns:mid1=\"http://schemas.datacontract.org/2004/07/Middleware.Adapter.DirectTopup.Resources.Messages.DirectTopupAdapter\">
+         <soapenv:Header>
+            <int:QueryTransactionReqHeader>
+               <mid:RequestId>#{request_id}</mid:RequestId>
+               <mid:Timestamp>#{Time.zone.now.strftime("%Y-%m-%d")}</mid:Timestamp>
+               <!--Optional:-->
+               <mid:SourceSystem>#{user_id}</mid:SourceSystem>
+               <mid:Credentials>
+                  <mid:User>#{user_id}</mid:User>
+                  <mid:Password>#{pass}</mid:Password>
+               </mid:Credentials>
+               <!--Optional:-->
+               <mid:Attributes>
+                  <!--Zero or more repetitions:-->
+                  <mid:Attribute>
+                     <mid:Name>?</mid:Name>
+                     <mid:Value>?</mid:Value>
+                  </mid:Attribute>
+               </mid:Attributes>
+            </int:QueryTransactionReqHeader>
+         </soapenv:Header>
+         <soapenv:Body>
+            <int:QueryTransactionReq>
+               <!--Optional:-->
+               <int:QueryTransactionReqBody>
+                  <mid1:TransactionNumber>?</mid1:TransactionNumber>
+               </int:QueryTransactionReqBody>
+            </int:QueryTransactionReq>
+         </soapenv:Body>
+      </soapenv:Envelope>
+    "
+
+    url = "#{url_service}/DirectTopupService/Topup/"
+    uri = URI.parse(URI.escape(url))
+    request = HTTParty.post(uri, 
+      :headers => {
+        'Content-Type' => 'text/xml;charset=UTF-8',
+        'SOAPAction' => 'http://ws.movicel.co.ao/middleware/adapter/DirectTopup/interface/DirectTopupService_Outbound/QueryTransaction',
+      },
+      :body => body
+    )
+
+    if (200...300).include?(request.code.to_i)
+      # return request.body
+      return Nokogiri::XML(request.body).children.children.children.children.children.children.text rescue nil
+      # return "#{request_id} - #{Nokogiri::XML(request.body).children.children.children.children.children.text}" rescue nil
     end
   end
 
