f8c5cf439e0490fb98172ce4973935c917bb233a
Danilo <didox_59@yahoo.com.br>
2020-04-03 07:01:32 -0300

    dados produtos

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -93,7 +93,7 @@ class PartnersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_partner
-      @partner = Partner.find(params[:id])
+      @partner = Partner.find(params[:id] || params[:partner_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
