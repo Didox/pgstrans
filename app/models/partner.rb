7c6f1f15dd057bc7349a9139e23f12bacb1f8147
Danilo <didox_59@yahoo.com.br>
2020-04-24 07:42:18 -0300

    api key zap

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -15,7 +15,8 @@ class Partner < ApplicationRecord
       "#{host}/ao/echarge/pagaso/dev/portfolio/menu", 
       headers: {
         apikey: api_key
-      }
+      },
+      verify: false 
     )
 
     if (200...300).include?(res.code)
