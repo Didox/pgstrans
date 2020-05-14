32fba40cec96acd4bf8bf504cae653e49666152c
Danilo <didox_59@yahoo.com.br>
2020-05-14 07:43:11 -0300

    tirando transação para http

--- app/models/lancamento.rb
+++ app/models/lancamento.rb
@@ -1,6 +1,22 @@
 class Lancamento < ApplicationRecord
 	validates :nome, presence: true, uniqueness: true
 
+  def save!
+    if self.nome == "Compra de crédito"
+      raise "Este status não pode ser alterado uso interno"
+    end
+
+    super
+  end
+
+  def save
+    if self.nome == "Compra de crédito"
+      raise "Este status não pode ser alterado uso interno"
+    end
+
+    super
+  end
+
   def destroy
     if self.nome != "Compra de crédito"
       super
