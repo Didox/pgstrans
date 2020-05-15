d1ab47b8ae78afcc6c07478162f11a1568d4d485
Danilo <didox_59@yahoo.com.br>
2020-05-15 08:08:57 -0300

    busca saldo zap

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -76,7 +76,7 @@ class Partner < ApplicationRecord
     end
 
     ###### Entender se realmente não precisamos passar este id
-    # partner.store_id_parceiro # "115356"
+    # partner.STORE_ID_ZAP_PARCEIRO # "115356"
     ############################################################
     data = Time.zone.now - 20.days
     while data <= Time.zone.now
@@ -128,98 +128,106 @@ class Partner < ApplicationRecord
     end
   end
 
-  def saldo_atual
-    if self.slug.downcase == "movicel"
-      require 'openssl'
+  def saldo_atual_movicel
+    parceiro = Partner.where("lower(slug) = 'movicel'").first
+    parametro = Parametro.where(partner_id: parceiro.id).first
 
-      #producao
-      agent_key = "3958CFE4F3A8DB09B76B9C0D26A19F6D69E1F6DC3BC5779EB84CB866DD939DF4"
-      url_service = "https://ws.movicel.co.ao:10073"
-
-      #homologacao
-      # agent_key = "5CF0AC45050B030B9E3A5FC14A5D7C8B609B2BDD40119B5C32539E1F3B6CEF7F"
-      # url_service = "http://wsqa.movicel.co.ao:10071"
-
-      user_id = "TivTechno"
-      request_id = Time.now.strftime("%d%m%Y%H%M%S")
-
-      pass = `AGENTKEY='#{agent_key}' USERID='#{user_id}' REQUESTID='#{request_id}' ./chaves/movicell/ubuntu/encripto`
-      # pass = `AGENTKEY='#{agent_key}' USERID='#{user_id}' REQUESTID='#{request_id}' ./chaves/movicell/mac/encripto`
-      pass = pass.strip
-
-      body = "
-        <soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:int=\"http://ws.movicel.co.ao/middleware/adapter/DirectTopup/interface\" xmlns:mid=\"http://schemas.datacontract.org/2004/07/Middleware.Common.Common\">
-         <soapenv:Header>
-            <int:QueryBalanceReqHeader>
-               <mid:RequestId>#{request_id}</mid:RequestId>
-               <mid:Timestamp>#{Time.zone.now.strftime("%Y-%m-%d")}</mid:Timestamp>
-               <!--Optional:-->
-               <mid:SourceSystem>#{user_id}</mid:SourceSystem>
-               <mid:Credentials>
-                  <mid:User>#{user_id}</mid:User>
-                  <mid:Password>#{pass}</mid:Password>
-               </mid:Credentials>
-               <!--Optional:-->
-               <mid:Attributes>
-                  <!--Zero or more repetitions:-->
-                  <mid:Attribute>
-                     <mid:Name>?</mid:Name>
-                     <mid:Value>?</mid:Value>
-                  </mid:Attribute>
-               </mid:Attributes>
-            </int:QueryBalanceReqHeader>
-         </soapenv:Header>
-         <soapenv:Body>
-            <int:QueryBalanceReq>
-               <!--Optional:-->
-               <int:QueryBalanceReqBody/>
-            </int:QueryBalanceReq>
-         </soapenv:Body>
-        </soapenv:Envelope>
-      "
-
-      url = "#{url_service}/DirectTopupService/Topup/"
-      uri = URI.parse(URI.escape(url))
-      request = HTTParty.post(uri, 
-        :headers => {
-          'Content-Type' => 'text/xml;charset=UTF-8',
-          'SOAPAction' => 'http://ws.movicel.co.ao/middleware/adapter/DirectTopup/interface/DirectTopupService_Outbound/QueryBalance',
-        },
-        :body => body
-      )
-
-      if (200...300).include?(request.code.to_i)
-        return Nokogiri::XML(request.body).children.children.children.children.children.children.text rescue nil
-      end
-    elsif self.slug.downcase == "zaptv"
-      parceiro = Partner.where("lower(slug) = 'zaptv'").first
-      parametro = Parametro.where(partner_id: parceiro.id).first
-
-      if Rails.env == "development"
-        host = "#{parametro.url_integracao_desenvolvimento}/portfolio"
-        api_key = parametro.api_key_zaptv_desenvolvimento
-      else
-        host = "#{parametro.url_integracao_producao}/portfolio"
-        api_key = parametro.api_key_zaptv_producao
-      end
-
-      store_id_parceiro = "115356"
-      url = "#{host}/saldo?code=#{store_id_parceiro}"
-      res = HTTParty.get(
-        url, 
-        headers: {
-          "apikey" => api_key,
-          "Content-Type" => "application/json"
-        }
-      )
-
-      if (200..300).include?(res.code)
-        return JSON.parse(res.body)["saldo"]
-      else
-        return 0
-      end
+    if Rails.env == "development"
+      url_service = parametro.url_integracao_desenvolvimento
+      agent_key = parametro.agent_key_movicel_desenvolvimento
+    else
+      url_service = parametro.url_integracao_producao
+      agent_key = parametro.agent_key_movicel_producao
     end
-  
+
+    host = url_service.to_s.gsub(/o\:.*/, "o")
+
+    require 'openssl'
+
+    user_id = "TivTechno"
+    request_id = Time.now.strftime("%d%m%Y%H%M%S")
+
+    pass = `AGENTKEY='#{agent_key}' USERID='#{user_id}' REQUESTID='#{request_id}' ./chaves/movicell/ubuntu/encripto`
+    # pass = `AGENTKEY='#{agent_key}' USERID='#{user_id}' REQUESTID='#{request_id}' ./chaves/movicell/mac/encripto`
+    pass = pass.strip
+
+    body = "
+      <soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:int=\"#{host}/middleware/adapter/DirectTopup/interface\" xmlns:mid=\"http://schemas.datacontract.org/2004/07/Middleware.Common.Common\">
+        <soapenv:Header>
+          <int:QueryBalanceReqHeader>
+              <mid:RequestId>#{request_id}</mid:RequestId>
+              <mid:Timestamp>#{Time.zone.now.strftime("%Y-%m-%d")}</mid:Timestamp>
+              <!--Optional:-->
+              <mid:SourceSystem>#{user_id}</mid:SourceSystem>
+              <mid:Credentials>
+                <mid:User>#{user_id}</mid:User>
+                <mid:Password>#{pass}</mid:Password>
+              </mid:Credentials>
+              <!--Optional:-->
+              <mid:Attributes>
+                <!--Zero or more repetitions:-->
+                <mid:Attribute>
+                    <mid:Name>?</mid:Name>
+                    <mid:Value>?</mid:Value>
+                </mid:Attribute>
+              </mid:Attributes>
+          </int:QueryBalanceReqHeader>
+        </soapenv:Header>
+        <soapenv:Body>
+          <int:QueryBalanceReq>
+              <!--Optional:-->
+              <int:QueryBalanceReqBody/>
+          </int:QueryBalanceReq>
+        </soapenv:Body>
+      </soapenv:Envelope>
+    "
+
+    url = "#{url_service}/DirectTopupService/Topup/"
+    uri = URI.parse(URI.escape(url))
+    request = HTTParty.post(uri, 
+      :headers => {
+        'Content-Type' => 'text/xml;charset=UTF-8',
+        'SOAPAction' => "#{host}/middleware/adapter/DirectTopup/interface/DirectTopupService_Outbound/QueryBalance",
+      },
+      :body => body
+    )
+
+    if (200...300).include?(request.code.to_i)
+      return Nokogiri::XML(request.body).children.children.children.children.children.children.text rescue nil
+    end
+  end
+
+  def saldo_atual_zaptv
+    parceiro = Partner.where("lower(slug) = 'zaptv'").first
+    parametro = Parametro.where(partner_id: parceiro.id).first
+
+    if Rails.env == "development"
+      host = "#{parametro.url_integracao_desenvolvimento}/saldo"
+      api_key = parametro.api_key_zaptv_desenvolvimento
+    else
+      host = "#{parametro.url_integracao_producao}/saldo"
+      api_key = parametro.api_key_zaptv_producao
+    end
+
+    STORE_ID_ZAP_PARCEIRO = "115356"
+    url = "#{host}/saldo?code=#{STORE_ID_ZAP_PARCEIRO}"
+    res = HTTParty.get(
+      url, 
+      headers: {
+        "apikey" => api_key,
+        "Content-Type" => "application/json"
+      }
+    )
+
+    if (200..300).include?(res.code)
+      return JSON.parse(res.body)["saldo"]
+    else
+      return 0
+    end
+  end
+
+  def saldo_atual
+    return this.send("saldo_atual_#{self.slug.downcase}")
   rescue Exception => err
     Rails.logger.info "::::#{err.message}::::"
   end
