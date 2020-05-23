6d578ff7f0e590efa58bc806a32dcf8fb7ee39ce
Danilo <didox_59@yahoo.com.br>
2020-05-23 06:34:41 -0300

    permissionamento

---
+++ app/models/permissionamento_dados.rb
@@ -0,0 +1,38 @@
+module PermissionamentoDados
+  def self.included(base)
+    base.class_eval do 
+      attr_accessor :responsavel
+      
+      validate :verifica_responsavel
+      after_validation :salvar_responsavel
+
+      def self.com_acesso(usuario)
+        self.joins("
+        inner join grupo_registros on 
+          grupo_registros.modelo = '#{self.to_s}' and 
+          grupo_registros.modelo_id = #{self.to_s.underscore.pluralize}.id and 
+          grupo_registros.grupo_id in (#{usuario.grupos_id.join(",")}) 
+        ").distinct
+      end
+
+      def salva_responsavel(_responsavel)
+        self.responsavel = _responsavel
+        self.save
+      end
+
+      private 
+        def verifica_responsavel
+          self.errors.add(:responsavel, "Responsável não definido") if responsavel.blank?
+        end
+
+        def salvar_responsavel
+          raise "Responsável não definido" if responsavel.blank?
+          
+          responsavel.grupo_usuarios.each do |gu|
+            grs = GrupoRegistro.where(grupo_id: gu.grupo_id, modelo: self.class.to_s, modelo_id: self.id)
+            GrupoRegistro.create(grupo_id: gu.grupo_id, modelo: self.class.to_s, modelo_id: self.id) if grs.count == 0
+          end
+        end
+    end
+  end
+end
