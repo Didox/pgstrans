17c5dea7bd5b8ae5b8e0306fdee76ae9d0f7c8fa
Rosi Volgarin <rosi.volgarin@gmail.com>
2020-07-05 14:31:17 +0100

    apresentacao vendas resumido e total

--- app/controllers/vendas_controller.rb
+++ app/controllers/vendas_controller.rb
@@ -38,7 +38,7 @@ class VendasController < ApplicationController
   end
 
   def resumido
-    @vendas = Vendas.where(usuario_id: usuario_logado.id)
+    @vendas = Venda.where(usuario_id: usuario_logado.id)
     vendas_busca
   end
 
