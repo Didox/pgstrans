fad456cbbc1f8329fd4bad7f56377d93bd713cc8
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-07-16 08:35:13 -0300

    melhorias

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def usuario_logado
   	Usuario.find(JSON.parse(cookies[:usuario_pgstrans_oauth])["id"])
   end
+
+  def formata_numero_duas_casas(numero)
+    number_to_currency(numero, :precision => 2).downcase.gsub(/kz|\./,"").gsub(",",".")
+  end
 end
