2f273b52b80bf81f8373639476c58f6402b63eda
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-08-31 07:15:23 -0300

    atualizando maquina danilo

--- db/migrate/20200830215734_remove_campos_produtos_valor_minimo_venda.rb
+++
@@ -1,8 +0,0 @@
-class RemoveCamposProdutosValorMinimoVenda < ActiveRecord::Migration[5.2]
-  def change
-  	remove_column :produtos, :valor_minimo_venda_telemovel
-	remove_column :produtos, :valor_minimo_venda_site  	
-	remove_column :produtos, :valor_minimo_venda_pos
-	remove_column :produtos, :valor_minimo_venda_tef 	
-  end
-end
