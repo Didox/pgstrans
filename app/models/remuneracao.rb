e32d42abb1bc907f589e65600c90d26d7e59df6b
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-09-11 07:48:19 -0300

    rotas e cadastro de itens na API

--- app/models/remuneracao.rb
+++ app/models/remuneracao.rb
@@ -1,7 +1,4 @@
 class Remuneracao < ApplicationRecord
   include PermissionamentoDados
   belongs_to :usuario
-  belongs_to :produto
-
-  validates :usuario, uniqueness: { scope: [:usuario_id, :produto_id] }
 end
