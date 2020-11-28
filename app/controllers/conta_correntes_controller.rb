cb4d2edd75b1673b6d364f199385bb189378e26b
Rosi Volgarin <rosi.volgarin@gmail.com>
2020-11-28 19:50:56 +0100

    alteração ordem de classificação conta corrente

--- app/controllers/conta_correntes_controller.rb
+++ app/controllers/conta_correntes_controller.rb
@@ -11,7 +11,7 @@ class ContaCorrentesController < ApplicationController
     end
 
     @conta_correntes = @conta_correntes.joins("inner join usuarios on usuarios.id = conta_correntes.usuario_id")
-    @conta_correntes = @conta_correntes.reorder("updated_at desc")
+    @conta_correntes = @conta_correntes.reorder("data_ultima_atualizacao_saldo desc")
     @conta_correntes = @conta_correntes.where("conta_correntes.data_alegacao_pagamento >= ?", params[:data_alegacao_pagamento].to_datetime.beginning_of_day) if params[:data_alegacao_pagamento].present?
     @conta_correntes = @conta_correntes.where("conta_correntes.data_ultima_atualizacao_saldo <= ?", params[:data_ultima_atualizacao_saldo].to_date.end_of_day) if params[:data_ultima_atualizacao_saldo].present?
     @conta_correntes = @conta_correntes.where("usuarios.nome ilike '%#{params[:nome]}%'") if params[:nome].present?
