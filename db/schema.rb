56b7c819318066f97d2a0a688a19f739a64f1976
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-12-10 08:30:00 -0300

    caso mambuco

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_102659) do
+ActiveRecord::Schema.define(version: 2020_12_10_111631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -373,6 +373,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_102659) do
     t.datetime "vigencia_fim"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "ativo", default: true
     t.index ["usuario_id"], name: "index_remuneracaos_on_usuario_id"
   end
 
