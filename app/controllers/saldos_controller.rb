0a9ffb17ce97892182a29bb67f131935ff7bd917
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-12-11 08:15:28 -0300

    data de alegação ordenada

--- app/controllers/saldos_controller.rb
+++ app/controllers/saldos_controller.rb
@@ -2,7 +2,7 @@ class SaldosController < ApplicationController
   def index
     @conta_correntes = ContaCorrente.com_acesso(usuario_logado)
     @conta_correntes = @conta_correntes.joins("inner join usuarios on usuarios.id = conta_correntes.usuario_id")
-    @conta_correntes = @conta_correntes.reorder("updated_at desc")
+    @conta_correntes = @conta_correntes.reorder("data_alegacao_pagamento desc")
     
     @conta_correntes = @conta_correntes.where("conta_correntes.data_alegacao_pagamento >= ?", params[:data_alegacao_pagamento_inicio].to_datetime.beginning_of_day) if params[:data_alegacao_pagamento_inicio].present?
     @conta_correntes = @conta_correntes.where("conta_correntes.data_alegacao_pagamento <= ?", params[:data_alegacao_pagamento_fim].to_date.end_of_day) if params[:data_alegacao_pagamento_fim].present?
