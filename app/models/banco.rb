4c818a9daf2edf841dc9d135d528dcf6a80023a6
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-02-25 09:03:03 -0300

    importar dados de conta corrente apagados

--- app/models/banco.rb
+++ app/models/banco.rb
@@ -1,5 +1,5 @@
 class Banco < ApplicationRecord
   include PermissionamentoDados
   validates :nome, :sigla, presence: true, uniqueness: true
-  has_many :conta_correntes, dependent: :destroy
+  has_many :conta_correntes
 end
