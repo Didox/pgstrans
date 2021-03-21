7a140c70ededdb0cdf9fce051773081fc0d19ea3
Rosi Volgarin <rosi.volgarin@gmail.com>
2021-03-21 16:44:55 -0300

    Inserindo filtro de busca na view index atividade esconomicas

--- app/controllers/industries_controller.rb
+++ app/controllers/industries_controller.rb
@@ -6,6 +6,10 @@ class IndustriesController < ApplicationController
   def index
     @industries = Industry.com_acesso(usuario_logado).order(descricao_seccao: :asc, descricao_divisao: :asc, descricao_grupo: :asc)
 
+    @industries = @industries.where("descricao_seccao ilike '%#{params[:descricao_seccao]}%'") if params[:descricao_seccao].present?
+    @industries = @industries.where("descricao_divisao ilike '%#{params[:descricao_divisao]}%'") if params[:descricao_divisao].present?
+    @industries = @industries.where("descricao_grupo ilike '%#{params[:descricao_grupo]}%'") if params[:descricao_grupo].present?
+
     options = {page: params[:page] || 1, per_page: 17}
     @industries = @industries.paginate(options)
   end
