5403681281daf337eab433e4e75407f1562d09c3
Rosi Volgarin <rosi.volgarin@gmail.com>
2021-04-07 06:54:09 -0300

    Inclusao de filtros de busca na index unitel sequences - fix busca id venda

--- app/controllers/unitel_sequences_controller.rb
+++ app/controllers/unitel_sequences_controller.rb
@@ -15,7 +15,7 @@ class UnitelSequencesController < ApplicationController
     @unitel_sequences = @unitel_sequences.reorder("sequence_id desc")
     @unitel_sequences = @unitel_sequences.where("created_at >= ?", params[:data_inicio].to_datetime.beginning_of_day) if params[:data_inicio].present?
     @unitel_sequences = @unitel_sequences.where("created_at <= ?", params[:data_fim].to_datetime.end_of_day) if params[:data_fim].present?
-    @unitel_sequences = @unitel_sequences.where("vendas_id = ?", params[:venda_id]) if params[:venda_id].present?
+    @unitel_sequences = @unitel_sequences.where("venda_id = ?", params[:venda_id]) if params[:venda_id].present?
     @unitel_sequences = @unitel_sequences.where("sequence_id = ?", params[:sequence_id]) if params[:sequence_id].present?
 
     @unitel_sequences_total = @unitel_sequences.count
