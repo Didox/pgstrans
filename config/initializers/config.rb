5f69ae5549bc265ffac5d717cf3b17439b8fcdfa
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-04-16 08:12:47 -0300

    aws config

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,6 +1,9 @@
 VERSION="v2"
 URL_SITE=ENV["URL_SITE"] || "https://recarga.pagaso.co.ao/"
 DEFAULT_TIMEOUT = ENV["DEFAULT_TIMEOUT"] || 60
+AWS_ID=ENV["AWS_ID"]
+AWS_KEY=ENV["AWS_KEY"]
+AWS_BUCKET=ENV["AWS_BUCKET"]
 
 String.class_eval do
   def to_f_ptBR
