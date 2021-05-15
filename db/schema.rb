5ef14ce7c8f243be0f89025e7ce268525c38f2aa
Rosi Volgarin <rosi.volgarin@gmail.com>
2021-05-15 11:43:12 -0300

    Drop table status_alegacao_pagamento

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_104603) do
+ActiveRecord::Schema.define(version: 2021_05_15_144053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -466,12 +466,6 @@ ActiveRecord::Schema.define(version: 2021_05_15_104603) do
   end
 
   create_table "status_alegacao_de_pagamentos", force: :cascade do |t|
-    t.string "nome"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "status_alegacao_pagamentos", force: :cascade do |t|
     t.string "nome"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
