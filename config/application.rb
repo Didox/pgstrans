fb897740f97274670ef1ab488cff88001d866087
Rosi Volgarin <rosi.volgarin@gmail.com>
2021-06-07 20:36:42 -0300

    timezone local WAT

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,8 @@ module Pgstrans
     config.load_defaults 5.2
 
     config.i18n.default_locale = :pt
-    #config.time_zone = 'UTC'
-    config.active_record.default_timezone = :local
+    config.time_zone = 'West Africa Time'
+    #config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
