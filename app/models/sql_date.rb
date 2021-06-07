4cb67afea8afae2d3c2b3780051fcce054b0d211
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-06-07 07:30:28 -0300

    fix date

--- app/models/sql_date.rb
+++ app/models/sql_date.rb
@@ -1,7 +1,7 @@
 class SqlDate
   def self.sql_parse(data)
   	if Rails.env == "production"
-  		return (data + 1.hours).strftime("%Y-%m-%d %H:%M:%S")
+  		return (data - 1.hours).strftime("%Y-%m-%d %H:%M:%S")
   	else
   		return data.strftime("%Y-%m-%d %H:%M:%S")
   	end
