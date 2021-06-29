17b91062bf69225da2748b24e5ab503a734042cc
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-06-29 08:05:10 -0300

    menu DSTV

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -261,3 +261,13 @@ $(document).ajaxStart(function() {
 $(document).ajaxComplete(function() {
   $(".modal_loader").hide();
 });
+
+var menuDSTV = function(self){
+  var menu = $(self).parent().find('.jsItensMenu');
+  if(menu.css("display") != "block"){
+    menu.show()
+  }
+  else{
+    menu.hide()
+  }
+}
