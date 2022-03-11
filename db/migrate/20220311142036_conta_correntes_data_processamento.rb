95f2a6b6bceecfef9a1a5368ad2478acdb4a34ad
Danilo Santos <danilo.aparecido.santos@gmail.com>
2022-03-11 11:44:15 -0300

    data processamento

--- db/migrate/20220311142036_conta_correntes_data_processamento.rb
+++ db/migrate/20220311142036_conta_correntes_data_processamento.rb
@@ -1,5 +1,5 @@
 class ContaCorrentesDataProcessamento < ActiveRecord::Migration[5.2]
   def change
-    add_column :conta_correntes, :data_processamento, :daatetime
+    add_column :conta_correntes, :data_processamento, :datetime
   end
 end
