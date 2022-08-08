8e8a273663798aedcd49c095b3251158f360d2b7
Rosi Volgarin <rosi.volgarin@gmail.com>
2022-08-08 10:26:42 -0300

    show africell

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ git push
 
 ansible-playbook -i ansible/hosts ansible/provisiona.yml -u pgsadmin --private-key ansible/id_rsa
 
-ssh -i ansible/id_rsa pgsadmin@172.26.10.6
+# ssh -i ansible/id_rsa pgsadmin@172.26.10.6
