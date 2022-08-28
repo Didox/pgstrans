36e5b17ddc87694db8d9fdf1ae0f5aa73eecf910
Rosi Volgarin <rosi.volgarin@gmail.com>
2022-08-28 14:16:21 -0300

    Implementacao da confirmacao de transacao Africell

--- app/controllers/africell_controller.rb
+++ app/controllers/africell_controller.rb
@@ -9,7 +9,18 @@ class AfricellController < ApplicationController
   end
 
   def confirmacao_transacao
-    #
+    if params[:target_msisdn].present?
+      @venda = Venda.where(customer_number: params[:target_msisdn], partner_id: Partner.africell.id).first
+    end
+    if params[:request_id].present?
+      @venda = Venda.where(request_id: params[:request_id], partner_id: Partner.africell.id)
+    end
+    if params[:created_at].present?
+      @venda = Venda.where(created_at: params[:created_at], partner_id: Partner.africell.id)
+    end
+  rescue Exception => e
+    flash[:error] = e.message
+    @info = []
   end
 
 end
