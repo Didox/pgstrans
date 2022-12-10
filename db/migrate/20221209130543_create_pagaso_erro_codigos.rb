5484cbc7af600f2b023b112749d2f1411df38ef4
Rosi Volgarin <rosi.volgarin@gmail.com>
2022-12-09 23:09:12 -0300

    Iniciando v2 da api - criando e formatando scaffold pagaso_erro_codigos fix

--- db/migrate/20221209130543_create_pagaso_erro_codigos.rb
+++ db/migrate/20221209130543_create_pagaso_erro_codigos.rb
@@ -3,7 +3,7 @@ class CreatePagasoErroCodigos < ActiveRecord::Migration[5.2]
     create_table :pagaso_erro_codigos do |t|
       t.string :de
       t.string :para
-      t.string :mensagem
+      t.string :messagem
 
       t.timestamps
     end
