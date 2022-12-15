5fddc6c71e9c59c79eca1ee66603197868b543b4
Rosi Volgarin <rosi.volgarin@gmail.com>
2022-12-15 16:56:43 -0300

    Evoluindo Helper Tester recarga DSTV

--- test/requests/recarga_v2/request_dstv_test.rb
+++ test/requests/recarga_v2/request_dstv_test.rb
@@ -6,12 +6,14 @@ class RequestTestDstv < ActiveSupport::TestCase
     test "Tentando fazer recarga v2 DSTV com usuário NÃO logado" do
         uri = URI("#{Helper.url}/api/v2/recarga/confirma/dstv.json")
         res = HTTParty.post(uri)
+        puts "===Tentando fazer recarga v2 DSTV com usuário NÃO logado===="
         puts "=============[#{res.code}]==============="
         
         assert_equal 401, res.code
 
         conteudo = res.parsed_response
 
+        puts "===Tentando fazer recarga v2 DSTV com usuário NÃO logado===="
         puts "=========[#{conteudo}]==============="
 
         assert_equal conteudo["message"], "Área restrita. Digite o login e palavra-passe para entrar." 
@@ -19,7 +21,7 @@ class RequestTestDstv < ActiveSupport::TestCase
         assert_equal conteudo["status"],  401
     end
 
-    test "Tentando fazer recarga v2 DSTV com usuário logado" do
+    test "Tentando fazer recarga v2 DSTV com usuário logado, mas com parametros inválidos" do
         uri = URI("#{Helper.url}/api/v2/recarga/confirma/dstv.json")
         res = HTTParty.post(uri, {
             headers: {
@@ -33,16 +35,43 @@ class RequestTestDstv < ActiveSupport::TestCase
             }
         })
 
-        debugger
+        assert_equal 400, res.code
         
+        conteudo = res.parsed_response
+
+        puts "===Tentando fazer recarga v2 DSTV com usuário logado, mas com parâmetros inválidos=="
+        puts "=========[#{conteudo}]=========="
+
+        assert conteudo["message"].present?
+        assert_equal conteudo["code"],  "PGS_ERRO_RECARGA_0001"
+        assert_equal conteudo["status"],  400
+    end
+
+    test "Tentando fazer recarga v2 DSTV com usuário logado e com parâmetros válidos" do
+        uri = URI("#{Helper.url}/api/v2/recarga/confirma/dstv.json")
+        res = HTTParty.post(uri, {
+            headers: {
+                'authorization': "Bearer #{Helper.token_login}"
+            },
+            body: {
+                "transacao_smartcard": "true",
+                "produto_id": "129",
+                "valor": "1600",
+                "dstv_number": "41306400502"
+            }
+        })
+
+        debugger
         assert_equal 200, res.code
 
+        conteudo = res.parsed_response
 
-        # conteudo = res.parsed_response
+        puts "===Tentando fazer recarga v2 DSTV com usuário logado e com parâmetros válidos==="
+        puts "=========[#{conteudo}]=========="
 
-        # assert conteudo["mensagem"].present?
-        # assert conteudo["status"] == 401
-        # assert conteudo["sucesso"] == false
+        assert conteudo["message"].present?
+        assert conteudo["code"]
+        assert conteudo["status"] == 401
     end
 
     
