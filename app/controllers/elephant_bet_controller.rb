f9aed94a5feace9759c300a9b47ae7ce02d76618
Rosi Volgarin <rosi.volgarin@gmail.com>
2022-12-28 10:39:17 -0300

    Ajuste na consulta voucher elephant bet

--- app/controllers/elephant_bet_controller.rb
+++ app/controllers/elephant_bet_controller.rb
@@ -13,7 +13,7 @@ class ElephantBetController < ApplicationController
             @venda = Venda.where(transaction_reference: params[:transaction_reference], partner_id: Partner.elephantbet.id).first
             @info = ElephantBet.consultar_voucher_reference(params[:transaction_reference])
          end
-         flash[:error] = "Nenhum voucher encontrado com o parämetro fornecido" if @info.blank? || @venda.blank?
+         #flash[:error] = "Nenhum voucher encontrado com o parämetro fornecido" if @info.blank? || @venda.blank?
          return
       end
 
@@ -22,7 +22,7 @@ class ElephantBetController < ApplicationController
             @venda = Venda.where(payment_code: params[:payment_code], partner_id: Partner.elephantbet.id).first
             @info = ElephantBet.consultar_voucher_payment_code(params[:payment_code])
          end
-         flash[:error] = "Nenhum voucher encontrado com o parämetros fornecido" if @info.blank? || @venda.blank?
+         #flash[:error] = "Nenhum voucher encontrado com o parämetros fornecido" if @info.blank? || @venda.blank?
       end
 
    rescue Exception => e
