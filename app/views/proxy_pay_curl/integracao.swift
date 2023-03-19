32b57e03b90d45524f8a2b4566fc36c95b289cf1
rosi.volgarin@gmail.com <rosi.volgarin@gmail.com>
2023-03-19 14:46:31 +0100

    Inclusao da Proxy nos Parametros de Integracao

---
+++ app/views/proxy_pay_curl/integracao.swift
@@ -0,0 +1,17 @@
+
+19/03/2023
+
+Authentication
+# Metodo GET
+
+## Desenvolvimento
+
+curl "https://developer.proxypay.co.ao/v2/" \
+  -H 'Authorization: Token omj8nk3oh7pvt6k5p9irq2ne4a34pl5i' \
+  -H 'Accept: application/vnd.proxypay.v2+json' \
+  -H 'Content-Type: application/json'
+
+
+
+
+
