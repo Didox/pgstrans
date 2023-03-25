ae6b3805a9fdb54ef89fd16da45ba1d84e877817
rosi.volgarin@gmail.com <rosi.volgarin@gmail.com>
2023-03-25 02:56:31 +0100

    Sequencia pagamento por referencia

--- app/views/proxy_pay_curl/integracao.swift
+++ app/views/proxy_pay_curl/integracao.swift
@@ -6,12 +6,28 @@ Authentication
 
 ## Desenvolvimento
 
+
 curl "https://developer.proxypay.co.ao/v2/" \
   -H 'Authorization: Token omj8nk3oh7pvt6k5p9irq2ne4a34pl5i' \
   -H 'Accept: application/vnd.proxypay.v2+json' \
   -H 'Content-Type: application/json'
 
 
+ https://api.sandbox.proxypay.co.ao/references/938849107
+
+## Registrar uma referencia
+
+curl -X PUT "https://api.sandbox.proxypay.co.ao/references/938849108" \
+  -H "Authorization: Token omj8nk3oh7pvt6k5p9irq2ne4a34pl5i" \
+  -H 'Accept: application/vnd.proxypay.v2+json'
 
 
+## https://app.sandbox.proxypay.co.ao/references
+
+
+## Apagar uma referencia
+
+  curl -X DELETE "https://api.sandbox.proxypay.co.ao/references/938849108" \
+  -H "Authorization: Token omj8nk3oh7pvt6k5p9irq2ne4a34pl5i" \
+  -H 'Accept: application/vnd.proxypay.v2+json'
 
