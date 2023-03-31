7659a7a2a143481814b02c5b988aa71b373070f7
rosi.volgarin@gmail.com <rosi.volgarin@gmail.com>
2023-03-31 20:43:21 +0100

    Curl payments

--- app/views/proxy_pay_curl/integracao.swift
+++ app/views/proxy_pay_curl/integracao.swift
@@ -31,3 +31,52 @@ curl -X PUT "https://api.sandbox.proxypay.co.ao////references/938849108" \
   -H "Authorization: Token omj8nk3oh7pvt6k5p9irq2ne4a34pl5i" \
   -H 'Accept: application/vnd.proxypay.v2+json'
 
+
+
+
+
+curl -H "Content-Type: application/json" -I -X POST http://localhost:3000/webhook/conciliacao_proxy_pay.json
+
+
+curl -d '{
+  "transaction_id": 26356,
+  "terminal_type": "IB",
+  "terminal_transaction_id": 0,
+  "terminal_location": null,
+  "terminal_id": "4644327306",
+  "reference_id": 111111139,
+  "product_id": null,
+  "period_start_datetime": "2007-03-13T19:00:00Z",
+  "period_id": 1562,
+  "period_end_datetime": "2007-03-14T19:00:00Z",
+  "parameter_id": 0,
+  "id": 156200026356,
+  "fee": "50.00",
+  "entity_id": 411,
+  "datetime": "2017-01-17T09:08:00Z",
+  "custom_fields": {},
+  "amount": "1000.01"
+}' -H "Content-Type: application/json" -H "X-Signature: b2faea0b081025ab55881e7684615c3920d2c67b4694c6ff770b5225c9fd464c" -X POST http://localhost:3000/webhook/conciliacao_proxy_pay.json
+
+
+170813645
+
+curl -d '{
+  "transaction_id": 26356,
+  "terminal_type": "IB",
+  "terminal_transaction_id": 0,
+  "terminal_location": null,
+  "terminal_id": "4644327306",
+  "reference_id": 170813645,
+  "product_id": null,
+  "period_start_datetime": "2023-03-30T19:00:00Z",
+  "period_id": 1562,
+  "period_end_datetime": "2023-03-30T19:00:00Z",
+  "parameter_id": 0,
+  "id": 156200026356,
+  "fee": "50.00",
+  "entity_id": 411,
+  "datetime": "2023-03-30T09:08:00Z",
+  "custom_fields": {},
+  "amount": "1000.01"
+}' -H "Content-Type: application/json" -H "X-Signature: b2faea0b081025ab55881e7684615c3920d2c67b4694c6ff770b5225c9fd464c" -X POST http://localhost:3000/webhook/conciliacao_proxy_pay.json
