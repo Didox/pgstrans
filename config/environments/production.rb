e54f6f21c25ed29aef51ea53680d7500f3cb0639
rosi.volgarin@gmail.com <rosi.volgarin@gmail.com>
2023-09-04 14:46:30 +0100

    revendo requisitos de secrets fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -121,8 +121,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  config.action_controller.default_url_options = {host: "www.yoursite.com"}
-  config.action_controller.asset_host = "www.yoursite.com"
+  #config.action_controller.default_url_options = {host: "www.yoursite.com"}
+  #config.action_controller.asset_host = "www.yoursite.com"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
